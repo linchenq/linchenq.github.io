f7c86a2308a442214d5e389c455707c5fb2954e8
linchenq <qianlc95@outlook.com>
2026-05-13 01:31:53 +0000

    Deploying to gh-pages from @ linchenq/linchenq.github.io@226ff507e55de3523dfd56a533d60fc69f030336 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-research",
+          title: "Research",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/research/";
+          },
         },{id: "nav-reviewer-experiences",
           title: "Reviewer Experiences",
           description: "",
